f8adc4a1e99b8ff3190c1b28a894b497ff27a38e
SardarbekRakhmankulov <sardarbek.rahmankulov@gmail.com>
2023-12-10 20:08:20 +0600

    Update Main.java
    longest substring

--- LeetCoding/src/Main.java
+++ LeetCoding/src/Main.java
@@ -65,4 +65,18 @@ public class Main {
             return x * myPow(x, n - 1);
         return myPow(x * x, n / 2);
     }
+
+    public int lengthOfLongestSubstring(String s) {
+        int ans = 0;
+        int[] count = new int[128];
+
+        for (int l = 0, r = 0; r < s.length(); ++r) {
+            ++count[s.charAt(r)];
+            while (count[s.charAt(r)] > 1)
+                --count[s.charAt(l++)];
+            ans = Math.max(ans, r - l + 1);
+        }
+
+        return ans;
+    }
 }
