349251b18d9d058b3e1663645be5c389140ea6a4
SardarbekRakhmankulov <sardarbek.rahmankulov@gmail.com>
2023-12-11 23:41:37 +0600

    Update Main.java
    reverse integer

--- LeetCoding/src/Main.java
+++ LeetCoding/src/Main.java
@@ -79,4 +79,15 @@ public class Main {
 
         return ans;
     }
+
+    public int reverse(int x) {
+        long ans = 0;
+
+        while (x != 0) {
+            ans = ans * 10 + x % 10;
+            x /= 10;
+        }
+
+        return (ans < Integer.MIN_VALUE || ans > Integer.MAX_VALUE) ? 0 : (int) ans;
+    }
 }
