c5f23b0e2f6b398173f3289fe311fc6220fedb10
SardarbekRakhmankulov <sardarbek.rahmankulov@gmail.com>
2023-12-12 23:22:20 +0600

    Update Main.java
    container with most water

--- LeetCoding/src/Main.java
+++ LeetCoding/src/Main.java
@@ -90,4 +90,21 @@ public class Main {
 
         return (ans < Integer.MIN_VALUE || ans > Integer.MAX_VALUE) ? 0 : (int) ans;
     }
+
+    public int maxArea(int[] height) {
+        int ans = 0;
+        int l = 0;
+        int r = height.length - 1;
+
+        while (l < r) {
+            final int minHeight = Math.min(height[l], height[r]);
+            ans = Math.max(ans, minHeight * (r - l));
+            if (height[l] < height[r])
+                ++l;
+            else
+                --r;
+        }
+
+        return ans;
+    }
 }
