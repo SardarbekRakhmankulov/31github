aa6c076bffc9d5bdae2692c872d94b5d6fc0f1a3
SardarbekRakhmankulov <sardarbek.rahmankulov@gmail.com>
2023-12-16 22:48:34 +0600

    Update Main.java
    plus one

--- LeetCoding/src/Main.java
+++ LeetCoding/src/Main.java
@@ -161,4 +161,18 @@ public class Main {
                 matrix[j][i] = temp;
             }
     }
+
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                ++digits[i];
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] ans = new int[digits.length + 1];
+        ans[0] = 1;
+        return ans;
+    }
 }
