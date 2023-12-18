56b6eaa941dc33af0dacd2ae4e4301efeb2d0389
SardarbekRakhmankulov <sardarbek.rahmankulov@gmail.com>
2023-12-18 23:45:12 +0600

    Update Main.java
    sort Colors

--- LeetCoding/src/Main.java
+++ LeetCoding/src/Main.java
@@ -191,4 +191,22 @@ public class Main {
         // l := the minimum number s.t. l * l > x
         return (int) l - 1;
     }
+
+    public void sortColors(int[] nums) {
+        int zero = -1;
+        int one = -1;
+        int two = -1;
+
+        for (final int num : nums)
+            if (num == 0) {
+                nums[++two] = 2;
+                nums[++one] = 1;
+                nums[++zero] = 0;
+            } else if (num == 1) {
+                nums[++two] = 2;
+                nums[++one] = 1;
+            } else {
+                nums[++two] = 2;
+            }
+    }
 }
